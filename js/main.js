49d7c13edad142ed9b4e38c9c00c4ff2dd8e2810
Muhib <khmuhib2013@gmail.com>
2022-02-16 20:02:30 +0600

    first time

---
+++ js/main.js
@@ -0,0 +1,126 @@
+$('.banner_main').mousemove(function (a) {
+    var mouseX = (a.pageX*-1/300);
+    var mouseY = (a.pageY*-1/300);
+
+    $('.banner_right_upper').css('background-position', mouseX+'px '+mouseY+'px');
+})
+
+
+
+$('.testimonial_slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.client_slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.follow_slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:7
+        }
+    }
+})
+
+var wow = new WOW(
+    {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null,    // optional scroll container selector, otherwise use window,
+        resetAnimation: true,     // reset animation on end (default is true)
+    }
+);
+wow.init();
+
+
+
+$('.banner_creative_agency a').magnificPopup({
+    type: 'iframe',
+    iframe: {
+        markup: '<div class="mfp-iframe-scaler">'+
+            '<div class="mfp-close"></div>'+
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+            '</div>',
+
+        patterns: {
+            youtube: {
+                index: 'youtube.com/',
+                id: 'v=',
+                src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+            },
+            vimeo: {
+                index: 'vimeo.com/',
+                id: '/',
+                src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+            },
+            gmaps: {
+                index: '//maps.google.',
+                src: '%id%&output=embed'
+            }
+
+        },
+
+        srcAction: 'iframe_src',
+    }
+});
+
+
+$('.follow_slider_main .item a').magnificPopup({
+    type: 'image',
+    gallery:{
+        enabled:true
+    }
+});
+
